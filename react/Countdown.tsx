fd8fb599661d847fbede4909e17e4cb9c9c84010
Wendel Sodre <wendel.sodre@itelios.com>
2020-04-24 11:55:32 -0300

    controle customizavel

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,18 +1,29 @@
 import React from 'react'
 
-interface CountdownProps {}
+interface CountdownProps {
+  targetDate: string
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
-  return <div>
-    <h1>Oi teste</h1>
-  </div>
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+  return (
+    <div>
+      <h1>{targetDate}</h1>
+    </div>
+  )
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    }
+  },
 }
 
 export default Countdown
