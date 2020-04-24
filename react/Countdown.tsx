2b37aee9025d6e50f5511d2798c7f73d16a6ccf3
Wendel Sodre <wendel.sodre@itelios.com>
2020-04-24 12:14:45 -0300

    Tick

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {TimeSplit} from './typings/global'
+import {tick} from './utils/time'
 
 interface CountdownProps {
   targetDate: string
 }
+const DEFAULT_TARGET = (new Date('2020-04-24')).toISOString()
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({targetDate = DEFAULT_TARGET}) => {
+
+  const [nomeVariavel, funcaoUse] = useState<TimeSplit> ({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  tick(targetDate, funcaoUse)
+
   return (
     <div>
-      <h1>{targetDate}</h1>
+      <h1>Itelios - Faltam {`${nomeVariavel.hours}:${nomeVariavel.minutes}:${nomeVariavel.seconds}`} para entrega do modulo B2B</h1>
     </div>
   )
 }
